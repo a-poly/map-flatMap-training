8e6513b7e103a4d9554ffe954b1ec3b0e4de9395
jfcorugedo <jfcorugedo@gmail.com>
2018-09-24 22:58:10 +0200

    splits several tests into another class and apply some renaming

--- src/test/java/com/ing/f2etraining/MapVsFlatMapTest.java
+++ src/test/java/com/ing/f2etraining/MapVsFlatMapTest.java
@@ -33,7 +33,7 @@ public class MapVsFlatMapTest {
     private static final Timeout TIMEOUT = new Timeout(Duration.create(5, "seconds"));
 
     @Test
-    public void whatIsAMapOnLists() {
+    public void useMapOnList() {
         //given
         List<Person> people = Arrays.asList(
                 new Person().setName("Juan").setAge(35),
@@ -42,6 +42,7 @@ public class MapVsFlatMapTest {
         );
 
         //when
+        //Hint: You have to use map
         List<String> names = people.stream()
                                 .map( person -> person.getName() )
                                 .collect(Collectors.toList());
@@ -51,11 +52,12 @@ public class MapVsFlatMapTest {
     }
 
     @Test
-    public void whatIsAMapOnOptionals() {
+    public void useMapOnOptional() {
         //given
         Optional<Person> personOptional = Optional.of(new Person().setName("Juan").setAge(35));
 
         //when
+        //Hint: You have to use map
         Optional<String> nameOptional = personOptional.map( person -> person.getName() );
 
         //then
@@ -63,7 +65,21 @@ public class MapVsFlatMapTest {
     }
 
     @Test
-    public void whatIsAFlatMapOnLists() {
+    public void useMaoOnFutures() throws Exception {
+        //given
+        Future<Person> personFuture = Futures.successful(new Person().setName("Juan").setAge(35));
+
+        //when
+        //Hint: You have to use map
+        Future<String> nameF = personFuture.map(person -> person.getName(), EXECUTOR);
+
+        //then
+        String name = (String) Await.result(nameF, TIMEOUT.duration());
+        assertThat(name).isEqualTo("Juan");
+    }
+
+    @Test
+    public void useFlatMapOnList() {
         //given
         List<Person> people = Arrays.asList(
                 new Person().setName("Juan").setAge(35),
@@ -72,6 +88,7 @@ public class MapVsFlatMapTest {
         );
 
         //when
+        //Hint: You have to use flatMap
         List<String> names = people.stream()
                                 .flatMap( person -> Stream.of(person.getName()) )
                                 .collect(Collectors.toList());
@@ -81,11 +98,12 @@ public class MapVsFlatMapTest {
     }
 
     @Test
-    public void whatIsAFlatMapOnOptionals() {
+    public void useFlatMapOnOptional() {
         //given
         Optional<Person> personOptional = Optional.of(new Person().setName("Juan").setAge(35));
 
         //when
+        //Hint: You have to use flatMap
         Optional<String> nameOptional = personOptional.flatMap( person -> Optional.ofNullable(person.getName()) );
 
         //then
@@ -93,175 +111,16 @@ public class MapVsFlatMapTest {
     }
 
     @Test
-    public void whatIsAMapOnFutures() throws Exception {
+    public void useFlatMapOnFuture() throws Exception {
         //given
         Future<Person> personFuture = Futures.successful(new Person().setName("Juan").setAge(35));
 
         //when
-        Future<String> nameF = personFuture.map(person -> person.getName(), EXECUTOR);
-
-        //then
-        String name = (String) Await.result(nameF, TIMEOUT.duration());
-        assertThat(name).isEqualTo("Juan");
-    }
-
-    @Test
-    public void whatIsAFlatMapOnFutures() throws Exception {
-        //given
-        Future<Person> personFuture = Futures.successful(new Person().setName("Juan").setAge(35));
-
-        //when
+        //Hint: You have to use flatMap
         Future<String> nameF = personFuture.flatMap(person -> Futures.successful(person.getName()), EXECUTOR);
 
         //then
         String name = (String) Await.result(nameF, TIMEOUT.duration());
         assertThat(name).isEqualTo("Juan");
-    }
-
-    @Test
-    public void combineSeveralFuturesWithFlatMap() throws Exception {
-        //given
-        Future<Person> meFuture = Futures.successful(new Person().setName("Juan").setAge(35));
-        Future<Person> friendFuture = Futures.successful(new Person().setName("Miguel").setAge(28));
-
-        //when
-        Future<Integer> sumAgeF = meFuture.flatMap(
-                me -> friendFuture.flatMap(
-                            friend -> Futures.successful(me.getAge() + friend.getAge())
-                , EXECUTOR)
-        , EXECUTOR);
-
-        //then
-        Integer sumAge = (Integer) Await.result(sumAgeF, TIMEOUT.duration());
-        assertThat(sumAge).isEqualTo(63);
-    }
-
-    @Test
-    public void combineSeveralFuturesWithMap() throws Exception {
-        //given
-        Future<Person> meFuture = Futures.successful(new Person().setName("Juan").setAge(35));
-        Future<Person> friendFuture = Futures.successful(new Person().setName("Miguel").setAge(28));
-
-        //when
-        Future<Future<Integer>> sumAgeFF = meFuture.map(
-                me -> friendFuture.map(
-                        friend -> friend.getAge() + me.getAge()
-                , EXECUTOR)
-        , EXECUTOR);
-
-        //then
-        Future<Integer> sumAgeF = Await.result(sumAgeFF, TIMEOUT.duration());
-        Integer sumAge = (Integer) Await.result(sumAgeF, TIMEOUT.duration());
-        assertThat(sumAge).isEqualTo(63);
-    }
-
-    @Test
-    public void combineSeveralFuturesWithFlatMapAndMap() throws Exception {
-        //given
-        Future<Person> meFuture = Futures.successful(new Person().setName("Juan").setAge(35));
-        Future<Person> friendFuture = Futures.successful(new Person().setName("Miguel").setAge(28));
-
-        //when
-        Future<Integer> sumAgeF = meFuture.flatMap(
-                me ->friendFuture.map(
-                        friend -> friend.getAge() + me.getAge()
-                , EXECUTOR)
-        , EXECUTOR);
-
-        //then
-        Integer sumAge = (Integer) Await.result(sumAgeF, TIMEOUT.duration());
-        assertThat(sumAge).isEqualTo(63);
-    }
-
-    @Test
-    public void combineSeveralDependentFuturesWithFlatMapAndMap() throws Exception {
-        //given
-        Future<Person> meFuture = Futures.successful(new Person().setName("Juan").setAge(35));
-        //Use service Future<Person> getFriend(String name)
-
-        //when
-        Future<Integer> sumAgeF = meFuture.flatMap(
-                person -> getFriend(person.getName()).map(
-                        friend -> person.getAge() + friend.getAge()
-                , EXECUTOR)
-            , EXECUTOR);
-
-        //then
-        Integer sumAge = (Integer) Await.result(sumAgeF, TIMEOUT.duration());
-        assertThat(sumAge).isEqualTo(63);
-    }
-
-    @Test
-    public void combineSeveralFuturesWithFailureResult() throws Exception {
-        //given
-        Future<Person> meFuture = Futures.failed(new Exception("Unexpected error"));
-        Future<Person> friendFuture = Futures.successful(new Person().setName("Miguel").setAge(28));
-
-        //when
-        /* TODO */
-        Future<Integer> sumAgeF = meFuture.flatMap(
-                me -> friendFuture.map(
-                        friend -> me.getAge() + friend.getAge()
-                        ,EXECUTOR
-                )
-                , EXECUTOR
-        ).recover(Java8.recoverF(error -> -1), EXECUTOR);
-
-        //then
-        Integer sumAge = (Integer) Await.result(sumAgeF, TIMEOUT.duration());
-        assertThat(sumAge).isEqualTo(-1);
-    }
-
-    @Test
-    public void combineSeveralFuturesWithAnotherFailureResult() throws Exception {
-        //given
-        Future<Person> meFuture = Futures.successful(new Person().setName("Juan").setAge(35));
-        Future<Person> friendFuture = Futures.failed(new Exception("Another unexpected error"));
-
-        //when
-        /* TODO */
-        Future<Integer> sumAgeF = meFuture.flatMap(
-                me -> friendFuture.map(
-                        friend -> me.getAge() + friend.getAge()
-                        ,EXECUTOR
-                )
-                , EXECUTOR
-        ).recover(Java8.recoverF(error -> -1), EXECUTOR);
-
-        //then
-        Integer sumAge = (Integer) Await.result(sumAgeF, TIMEOUT.duration());
-        assertThat(sumAge).isEqualTo(-1);
-    }
-
-    @Test
-    public void combineSeveralFuturesWithALeftResult() throws Exception {
-        //given
-        Future<Either<GenericError, Person>> meFuture = Futures.successful(new Right<>(new Person().setName("Juan").setAge(35)));
-        Future<Either<GenericError, Person>> friendFuture = Futures.successful(new Left<>(new MyError("Invalid user")));
-
-        //when
-        /* TODO */
-        Future<Integer> sumAgeF = meFuture.flatMap(
-                me -> friendFuture.map(
-                        friend -> {
-                            if(me.isLeft() || friend.isLeft()) {
-                                return -2;
-                            }
-                            return me.right().get().getAge() + friend.right().get().getAge();
-                        }
-                        ,EXECUTOR
-                )
-                , EXECUTOR
-        ).recover(Java8.recoverF(error -> -1), EXECUTOR);
-
-        //then
-        Integer sumAge = (Integer) Await.result(sumAgeF, TIMEOUT.duration());
-        assertThat(sumAge).isEqualTo(-2);
-    }
-
-
-    private Future<Person> getFriend(String name) {
-
-        return Futures.successful(new Person().setName("Miguel").setAge(28));
     }
 }
